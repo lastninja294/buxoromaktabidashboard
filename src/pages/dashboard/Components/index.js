9f5c8d64b4369bba01eaceb5bcaecc1f03193e49
beckoff23 <lastninja294@gmail.com>
2022-11-13 11:50:23 +0500

    Fixed

--- src/pages/dashboard/Components/index.js
+++ src/pages/dashboard/Components/index.js
@@ -1,4 +1,4 @@
-import RichTextEditor from './FormElements/RichTextEditor';
+import RichTextEditor from './FormElements';
 import PaginationForTable from './Pagination';
 
 export {RichTextEditor, PaginationForTable};
